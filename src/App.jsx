6e46f21a55d222ae2623cea7dc290f844b858185
staystale <georgesspooner@gmail.com>
2020-01-21 20:47:59 +0000

    remove shit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>HelloBillandfriends</h1>
+        <h1>Hello</h1>
         <Button variant="primary">Primary</Button>
       </div>
     );
